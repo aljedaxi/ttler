d5417f4b9e240fcb76978d0d67143a717a419811
jacob whitford-bender <jacob@xmentium.com>
2025-01-09 00:06:31 -0700

    build: added mac os via flake-utils.eachDefaultSystem

--- flake.nix
+++ flake.nix
@@ -1,25 +1,24 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs =
-    { nixpkgs, self, ... }: let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-        devShells."${system}".default = pkgs.mkShell {
-            packages = with pkgs; [gnumake nodejs];
-            shellHook = ''exec zsh'';
-        };
-        packages."${system}".default = pkgs.buildNpmPackage {
-	     dontNpmBuild = true;
-	     name = "swag";
-         npmDepsHash = "sha256-w8ZEAK8rWAPYlJ5RhHml6iwgsM4YRpzX+6ExuORWki8=";
-	     src = ./.;
-        };
-        apps."${system}".default = {
-	     type = "app";
-	     program = "${self.packages."${system}".default}/lib/node_modules/ttlr/main.js";
-        };
-    };
+  outputs = { nixpkgs, self, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system}; in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [gnumake nodejs];
+        shellHook = ''exec zsh'';
+      };
+      packages.default = pkgs.buildNpmPackage {
+        dontNpmBuild = true;
+        name = "ttlr";
+        npmDepsHash = "sha256-w8ZEAK8rWAPYlJ5RhHml6iwgsM4YRpzX+6ExuORWki8=";
+        src = ./.;
+      };
+      apps.default = {
+	      type = "app";
+	      program = "${self.packages."${system}".default}/lib/node_modules/ttlr/main.js";
+      };
+    });
 }
